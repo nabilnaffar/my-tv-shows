df52f231c5915369a066241a4575cec959078491
Nabil Naffer <naffar.nabil@gmail.com>
2017-06-20 17:35:54 +0300

    Facebook

--- cut-n-share/server/public/mobile/app.js
+++ cut-n-share/server/public/mobile/app.js
@@ -1,18 +1,57 @@
-console.log('loaded');
-document.getElementById('ok').style.display = 'none';
+function init(){
+    console.log('loaded');
+    document.getElementById('ok').style.display = 'none';
 
-const socket = io('/');
-socket.on('connect', function () {
-    console.log('Mobile is connected!');
-    socket.emit('client_info', {type: 'mobile'});
-});
+    const socket = io('/');
+    socket.on('connect', function () {
+        console.log('Mobile is connected!');
+        socket.emit('client_info', {type: 'mobile'});
+    });
 
-socket.on('edit_video', ()=>{
-    document.getElementById('title').innerHTML = 'Editing video...';
-    document.getElementById('ok').style.display = 'block';
-});
+    socket.on('edit_video', ()=>{
+        document.getElementById('title').innerHTML = 'Editing video...';
+        document.getElementById('ok').style.display = 'block';
+    });
+
+    
+}
+
+function loginToFB() {
+    FB.login(function (response) {
+        if (response.authResponse) {
+            console.log('Welcome!  Fetching your information.... ');
+            FB.api('/me', function (response) {
+                console.log('Good to see you, ' + response.name + '.');
+            });
+        } else {
+            console.log('User cancelled login or did not fully authorize.');
+        }
+    }, {
+            scope: 'publish_actions',
+            return_scopes: true
+    });
+}
+
+function postVideo(){
+
+}
+
+
 
 function done(){
     document.getElementById('title').innerHTML = 'Mobile app';
     document.getElementById('ok').style.display = 'none';
 }
+
+
+
+window.fbAsyncInit = function() {
+    console.log('!!!');
+    FB.init({
+      appId            : '1973039616261193',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v2.9'
+    });
+    FB.AppEvents.logPageView();
+  };
